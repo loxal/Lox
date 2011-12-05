964cf6bce957749163e3ad5ce9db126fa1cdf18a
Alexander Orlov <alexander.orlov@loxal.net>
2011-12-05 22:10:16 +0100

    generic DataStoreObject

---
+++ src/main/java/loxal/lox/service/meta/model/Task.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2011 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package loxal.lox.service.meta.model;
+
+import com.googlecode.objectify.annotation.Entity;
+
+/**
+ * @author Alexander Orlov <alexander.orlov@loxal.net>
+ */
+@Entity
+public class Task extends DatastoreObject { // "Problem"?
+    private String name;
+    private String description;
+    private String userEmail;
+    private Integer priority;
+    private String category;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
