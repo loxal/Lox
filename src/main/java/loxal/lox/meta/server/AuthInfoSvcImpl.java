60b544d240f4453bc2119d5659e3a14e33f05f0b
Alexander Orlov <alexander.orlov@loxal.net>
2011-12-05 16:01:35 +0100

    Lox deployed

--- src/main/java/loxal/lox/meta/server/AuthInfoSvcImpl.java
+++ src/main/java/loxal/lox/meta/server/AuthInfoSvcImpl.java
@@ -1,17 +1,7 @@
 /*
- * Copyright 2011 Alexander Orlov <alexander.orlov@loxal.net>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2011 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
  */
 
 package loxal.lox.meta.server;
@@ -28,10 +18,10 @@ import loxal.lox.service.meta.client.meta.authentication.AuthInfoSvc;
  */
 public class AuthInfoSvcImpl extends RemoteServiceServlet implements AuthInfoSvc {
     @Override
-    public AuthInfo getAuthInfo(String requestUri) {
-        UserService userService = UserServiceFactory.getUserService();
-        User user = userService.getCurrentUser();
-        AuthInfo auth = new AuthInfo();
+    public AuthInfo getAuthInfo(final String requestUri) {
+        final UserService userService = UserServiceFactory.getUserService();
+        final User user = userService.getCurrentUser();
+        final AuthInfo auth = new AuthInfo();
 
         if (user != null) {
             auth.setLoggedIn(true);
