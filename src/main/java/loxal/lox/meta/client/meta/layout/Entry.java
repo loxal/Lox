e376d52bdc260ae6acba225105d6e8215f8b1929
Alexander Orlov <alexander.orlov@loxal.net>
2012-01-09 16:24:25 +0100

    google api works

--- src/main/java/loxal/lox/meta/client/meta/layout/Entry.java
+++ src/main/java/loxal/lox/meta/client/meta/layout/Entry.java
@@ -16,23 +16,50 @@
 
 package loxal.lox.meta.client.meta.layout;
 
+import com.google.api.gwt.client.impl.ClientGoogleApiRequestTransport;
+import com.google.api.gwt.client.impl.ClientOAuth2Login;
+import com.google.api.gwt.services.plus.shared.Plus;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
+import loxal.lox.meta.client.meta.shared.PlusBase;
 
-public class Entry implements EntryPoint {
+public class Entry extends PlusBase implements EntryPoint {
     interface Binder extends UiBinder<Widget, Entry> {
     }
 
     private Binder binder = GWT.create(Binder.class);
 
     @Override
+    protected Plus createPlus() {
+        return GWT.create(Plus.class);
+    }
+
+    @Override
+    protected void println(String value) {
+        RootPanel.get().add(new Label(value));
+    }
+
+    @Override
     public void onModuleLoad() {
         Widget app = binder.createAndBindUi(this);
-        Window.setTitle("TEST");
-        RootLayoutPanel.get().add(app);
+//        Window.setTitle("TEST123");
+//        RootLayoutPanel.get().add(app);
+
+        final Button b = new Button("Authenticate to get public activities");
+        b.addClickHandler(new ClickHandler() {
+            @Override
+            public void onClick(ClickEvent event) {
+                login(new ClientOAuth2Login(CLIENT_ID), new ClientGoogleApiRequestTransport());
+                b.setVisible(false);
+            }
+        });
+        RootPanel.get().add(b);
     }
 }
